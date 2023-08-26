01b1ae90fd464af7d652bb3484f841b24cb1ae6a
AliMarwana <64220279+AliMarwana@users.noreply.github.com>
2023-08-26 23:29:40 +0100

    Add files via upload
    Commit of the project

--- App.mjs
+++ App.mjs
@@ -1,5 +1,8 @@
 //Here is an example of how to use the class Obstruction
-import { ObstructionModule } from "./ObstructionModule.mjs";
+//To test the ObstructionModule, type node App.mjs in the project folder with NodeJs installed
+// We set the value of timeDuration to 70 in the ObstructionModule
+import { IsObstructionImpenetrable, Location } from "./ObstructionModule.mjs";
+//IsObstructionImpenetrable = require("./ObstructionModule.mjs");
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 
@@ -9,10 +12,11 @@ var X1= await rl.question('X coordinate of Location A? ');
 var Y1= await rl.question('Y coordinate of Location A? ');
 var X2= await rl.question('X coordinate of Location B? ');
 var Y2= await rl.question('Y coordinate of Location B? ');
-var speed= await rl.question('Speed of the device? ');
+var speed= await rl.question('Speed of the device(mile/min)? ');
 var locationA = new Location(parseFloat(X1),parseFloat(Y1));
 var locationB = new Location(parseFloat(X2), parseFloat(Y2));
-var result = ObstructionModule.IsObstructionImpenetrable(locationA,locationB,speed);
+var result = IsObstructionImpenetrable(locationA,locationB,speed);
 console.log("is there a presence of impenetreable obstruction?:" + result);
+rl.close();
 
 
--- ObstructionModule.mjs
+++ ObstructionModule.mjs
@@ -1,8 +1,7 @@
-import math from "mathjs";
-import {sqrt, pow, divide} from math;
-//This value is supposed to be exported from the TimeDuration module
-var TimeDuration = 40;
-class Location
+
+import {sqrt, pow, divide, mode} from "mathjs";
+
+export class Location
 {
     constructor(x,y) //The constructor has 2 parameters: x and y for the location.
     {
@@ -11,12 +10,14 @@ class Location
     }
 }
 
-//This arrow function below determines whether there is an impenetrable obstruction
-IsObstructionImpenetrable = (locationA, locationB, speed) =>{
+//This function below determines whether there is an impenetrable obstruction
+export function IsObstructionImpenetrable (locationA, locationB, speed) {
     //The calculation of the distacne by using coordinates is given here
     var distance = sqrt(pow(locationB.x - locationA.x, 2) + pow(locationB.y - locationA.y,2)) ;
     //We calculate the time by dividing the distance to the speed
     var time = divide(distance, speed);
+    //This value is supposed to be exported from the TimeDuration module
+var TimeDuration = 70;
     if(TimeDuration - time > 60)
     {
         return true;
@@ -26,5 +27,6 @@ IsObstructionImpenetrable = (locationA, locationB, speed) =>{
         return false;
     }
 }
+
 
 
